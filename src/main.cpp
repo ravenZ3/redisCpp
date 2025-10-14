99b6edce9763fa584514ca6108bfd0eb86c96e70
ravenZ3 <ramjanthebest@gmail.com>
2025-10-14 10:46:35 +0530

    basic RESP array parsing for ECHO AND PING

--- src/main.cpp
+++ src/main.cpp
@@ -6,66 +6,79 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <netdb.h>
 #include <thread>
-#include <mutex>
-#include <condition_variable>
+#include <vector>
+#include <bits/stdc++.h>
 
 void DoWork(int client_fd);
 
-int main(int argc, char **argv)
+std::vector<std::string> f(const std::string &s)
 {
-  // Flush after every std::cout / std::cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  std::vector<std::string> tokens;
+  int i = 1; // skip '*'
+  int arr_count = 0;
 
+  while (isdigit(s[i]))
+  {
+    arr_count = arr_count * 10 + (s[i] - '0');
+    i++;
+  }
+  i += 2; // skip \r\n
+
+  for (int j = 0; j < arr_count; ++j)
+  {
+    if (s[i] != '$')
+      break;
+    i++; // skip $
+    int len = 0;
+    while (isdigit(s[i]))
+    {
+      len = len * 10 + (s[i] - '0');
+      i++;
+    }
+    i += 2; // skip \r\n
+    std::string curr = s.substr(i, len);
+    tokens.push_back(curr);
+    i += len + 2; // skip data + \r\n
+  }
+  return tokens;
+}
+int main()
+{
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0)
   {
-    std::cerr << "Failed to create server socket\n";
+    std::cerr << "socket failed\n";
     return 1;
   }
 
-  // Since the tester restarts your program quite often, setting SO_REUSEADDR
-  // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
-  {
-    std::cerr << "setsockopt failed\n";
-    return 1;
-  }
+  setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
 
-  struct sockaddr_in server_addr;
+  sockaddr_in server_addr{};
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(6379);
 
-  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
+  if (bind(server_fd, (sockaddr *)&server_addr, sizeof(server_addr)) != 0)
   {
-    std::cerr << "Failed to bind to port 6379\n";
+    std::cerr << "bind failed\n";
     return 1;
   }
 
-  int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0)
-  {
-    std::cerr << "listen failed\n";
-    return 1;
-  }
+  listen(server_fd, 5);
 
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  std::cout << "Waiting for a client to connect...\n";
+  sockaddr_in client_addr{};
+  socklen_t client_len = sizeof(client_addr);
+  std::cout << "Waiting for clients...\n";
 
   while (true)
   {
-    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
-    std::thread worker(DoWork, client_fd);
-    worker.detach();
+    int client_fd = accept(server_fd, (sockaddr *)&client_addr, &client_len);
+    std::thread(DoWork, client_fd).detach();
   }
 
   close(server_fd);
-
   return 0;
 }
 
@@ -74,15 +87,34 @@ void DoWork(int client_fd)
   char buffer[1024];
   while (true)
   {
-    ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer));
+    ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
     if (bytes_read <= 0)
       break;
 
-    // remove trailing \r\n
-    buffer[strcspn(buffer, "\r\n")] = '\0';
+    buffer[bytes_read] = '\0';
+    auto tokens = f(buffer);
+    // std::cout << "This is your server revciving data from client: " << client_fd << std::endl;
+    // for( auto i : tokens) std::cout << i << std::endl;
 
-    const char *pong = "+PONG\r\n";
-    send(client_fd, pong, strlen(pong), 0);
+    if (!tokens.empty())
+    {
+      if (tokens.size() == 1 && tokens[0] == "PING")
+      {
+        const char *pong = "+PONG\r\n";
+        send(client_fd, pong, strlen(pong), 0);
+      }
+      else if (tokens.size() >= 2 && tokens[0] == "ECHO")
+      {
+        std::string msg = tokens[1];
+        std::string resp = "+" + msg + "\r\n";
+        send(client_fd, resp.c_str(), resp.size(), 0);
+      }
+      else
+      {
+        const char *err = "-ERR unknown cmd\r\n";
+        send(client_fd, err, strlen(err), 0);
+      }
+    }
   }
   close(client_fd);
 }
