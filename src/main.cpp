e72b226763b2c5fb246c02191921bda013cf9c1e
ravenZ3 <ramjanthebest@gmail.com>
2025-10-14 11:07:40 +0530

    added support for GET and SET commands

--- src/main.cpp
+++ src/main.cpp
@@ -12,37 +12,10 @@
 
 void DoWork(int client_fd);
 
-std::vector<std::string> f(const std::string &s)
-{
-  std::vector<std::string> tokens;
-  int i = 1; // skip '*'
-  int arr_count = 0;
+std::vector<std::string> f(const std::string &s);
 
-  while (isdigit(s[i]))
-  {
-    arr_count = arr_count * 10 + (s[i] - '0');
-    i++;
-  }
-  i += 2; // skip \r\n
+std::unordered_map<std::string, std::string> m;
 
-  for (int j = 0; j < arr_count; ++j)
-  {
-    if (s[i] != '$')
-      break;
-    i++; // skip $
-    int len = 0;
-    while (isdigit(s[i]))
-    {
-      len = len * 10 + (s[i] - '0');
-      i++;
-    }
-    i += 2; // skip \r\n
-    std::string curr = s.substr(i, len);
-    tokens.push_back(curr);
-    i += len + 2; // skip data + \r\n
-  }
-  return tokens;
-}
 int main()
 {
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -94,7 +67,8 @@ void DoWork(int client_fd)
     buffer[bytes_read] = '\0';
     auto tokens = f(buffer);
     // std::cout << "This is your server revciving data from client: " << client_fd << std::endl;
-    // for( auto i : tokens) std::cout << i << std::endl;
+    // for (auto i : tokens)
+    //   std::cout << i << std::endl;
 
     if (!tokens.empty())
     {
@@ -103,12 +77,34 @@ void DoWork(int client_fd)
         const char *pong = "+PONG\r\n";
         send(client_fd, pong, strlen(pong), 0);
       }
-      else if (tokens.size() >= 2 && tokens[0] == "ECHO")
+      else if (tokens.size() == 2 && tokens[0] == "ECHO")
       {
         std::string msg = tokens[1];
         std::string resp = "+" + msg + "\r\n";
         send(client_fd, resp.c_str(), resp.size(), 0);
       }
+      else if (tokens.size() == 3 && tokens[0] == "SET")
+      {
+        m[tokens[1]] = tokens[2];
+        const char *okay = "+OK\r\n";
+        send(client_fd, okay, strlen(okay), 0);
+      }
+      else if (tokens.size() == 2 && tokens[0] == "GET")
+      {
+        auto it = m.find(tokens[1]);
+        if (it != m.end())
+        {
+          std::string msg = m[tokens[1]];
+          std::string resp = "+" + msg + "\r\n";
+          send(client_fd, resp.c_str(), resp.size(), 0);
+        }
+        else
+        {
+          const char *not_found = "-1\r\n";
+          send(client_fd, not_found, strlen(not_found), 0);
+        }
+      }
+
       else
       {
         const char *err = "-ERR unknown cmd\r\n";
@@ -117,4 +113,36 @@ void DoWork(int client_fd)
     }
   }
   close(client_fd);
+}
+
+std::vector<std::string> f(const std::string &s)
+{
+  std::vector<std::string> tokens;
+  int i = 1; // skip '*'
+  int arr_count = 0;
+
+  while (isdigit(s[i]))
+  {
+    arr_count = arr_count * 10 + (s[i] - '0');
+    i++;
+  }
+  i += 2; // skip \r\n
+
+  for (int j = 0; j < arr_count; ++j)
+  {
+    if (s[i] != '$')
+      break;
+    i++; // skip $
+    int len = 0;
+    while (isdigit(s[i]))
+    {
+      len = len * 10 + (s[i] - '0');
+      i++;
+    }
+    i += 2; // skip \r\n
+    std::string curr = s.substr(i, len);
+    tokens.push_back(curr);
+    i += len + 2; // skip data + \r\n
+  }
+  return tokens;
 }
