2a77b4121f35b04992345fe18dda4cba23e44862
ravenZ3 <ramjanthebest@gmail.com>
2025-10-14 11:26:01 +0530

    multithreading with mutex for SET PX subcommand

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstdlib>
 #include <string>
 #include <cstring>
 #include <unistd.h>
@@ -8,22 +7,21 @@
 #include <arpa/inet.h>
 #include <thread>
 #include <vector>
-#include <bits/stdc++.h>
-
-void DoWork(int client_fd);
-
-std::vector<std::string> f(const std::string &s);
+#include <unordered_map>
+#include <mutex>
+#include <chrono>
 
 std::unordered_map<std::string, std::string> m;
+std::mutex mtx;
+
+void DoWork(int client_fd);
+std::vector<std::string> f(const std::string &s);
 
 int main()
 {
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0)
-  {
-    std::cerr << "socket failed\n";
     return 1;
-  }
 
   int reuse = 1;
   setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
@@ -34,19 +32,15 @@ int main()
   server_addr.sin_port = htons(6379);
 
   if (bind(server_fd, (sockaddr *)&server_addr, sizeof(server_addr)) != 0)
-  {
-    std::cerr << "bind failed\n";
     return 1;
-  }
-
   listen(server_fd, 5);
 
-  sockaddr_in client_addr{};
-  socklen_t client_len = sizeof(client_addr);
   std::cout << "Waiting for clients...\n";
 
   while (true)
   {
+    sockaddr_in client_addr{};
+    socklen_t client_len = sizeof(client_addr);
     int client_fd = accept(server_fd, (sockaddr *)&client_addr, &client_len);
     std::thread(DoWork, client_fd).detach();
   }
@@ -58,6 +52,7 @@ int main()
 void DoWork(int client_fd)
 {
   char buffer[1024];
+
   while (true)
   {
     ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
@@ -66,83 +61,91 @@ void DoWork(int client_fd)
 
     buffer[bytes_read] = '\0';
     auto tokens = f(buffer);
-    // std::cout << "This is your server revciving data from client: " << client_fd << std::endl;
-    // for (auto i : tokens)
-    //   std::cout << i << std::endl;
 
-    if (!tokens.empty())
+    if (tokens.empty())
+      continue;
+
+    if (tokens.size() == 1 && tokens[0] == "PING")
     {
-      if (tokens.size() == 1 && tokens[0] == "PING")
+      const char *pong = "+PONG\r\n";
+      send(client_fd, pong, strlen(pong), 0);
+    }
+    else if (tokens.size() == 2 && tokens[0] == "ECHO")
+    {
+      std::string msg = "+" + tokens[1] + "\r\n";
+      send(client_fd, msg.c_str(), msg.size(), 0);
+    }
+    else if (tokens[0] == "SET")
+    {
+      std::string key = tokens[1];
+      std::string value = tokens[2];
+
       {
-        const char *pong = "+PONG\r\n";
-        send(client_fd, pong, strlen(pong), 0);
-      }
-      else if (tokens.size() == 2 && tokens[0] == "ECHO")
-      {
-        std::string msg = tokens[1];
-        std::string resp = "+" + msg + "\r\n";
-        send(client_fd, resp.c_str(), resp.size(), 0);
-      }
-      else if (tokens.size() == 3 && tokens[0] == "SET")
-      {
-        m[tokens[1]] = tokens[2];
-        const char *okay = "+OK\r\n";
-        send(client_fd, okay, strlen(okay), 0);
-      }
-      else if (tokens.size() == 2 && tokens[0] == "GET")
-      {
-        auto it = m.find(tokens[1]);
-        if (it != m.end())
-        {
-          std::string msg = m[tokens[1]];
-          std::string resp = "+" + msg + "\r\n";
-          send(client_fd, resp.c_str(), resp.size(), 0);
-        }
-        else
-        {
-          const char *not_found = "-1\r\n";
-          send(client_fd, not_found, strlen(not_found), 0);
-        }
+        std::lock_guard<std::mutex> lock(mtx);
+        m[key] = value;
       }
 
-      else
+      const char *okay = "+OK\r\n";
+      send(client_fd, okay, strlen(okay), 0);
+
+      if (tokens.size() > 3 && tokens[3] == "PX")
       {
-        const char *err = "-ERR unknown cmd\r\n";
-        send(client_fd, err, strlen(err), 0);
+        int time = std::stoi(tokens[4]);
+        std::thread([key, time]()
+                    {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(time));
+                    std::lock_guard<std::mutex> lock(mtx);
+                    m.erase(key); })
+            .detach();
       }
     }
+    else if (tokens.size() == 2 && tokens[0] == "GET")
+    {
+      std::lock_guard<std::mutex> lock(mtx);
+      auto it = m.find(tokens[1]);
+      if (it != m.end())
+      {
+        std::string msg = "$" + std::to_string(it->second.size()) + "\r\n" + it->second + "\r\n";
+        send(client_fd, msg.c_str(), msg.size(), 0);
+      }
+      else
+      {
+        const char *nil = "$-1\r\n";
+        send(client_fd, nil, strlen(nil), 0);
+      }
+    }
+    else
+    {
+      const char *err = "-ERR unknown cmd\r\n";
+      send(client_fd, err, strlen(err), 0);
+    }
   }
+
   close(client_fd);
 }
 
 std::vector<std::string> f(const std::string &s)
 {
   std::vector<std::string> tokens;
-  int i = 1; // skip '*'
+  int i = 1;
   int arr_count = 0;
-
   while (isdigit(s[i]))
-  {
-    arr_count = arr_count * 10 + (s[i] - '0');
-    i++;
-  }
-  i += 2; // skip \r\n
+    arr_count = arr_count * 10 + (s[i++] - '0');
+  i += 2;
 
   for (int j = 0; j < arr_count; ++j)
   {
     if (s[i] != '$')
       break;
-    i++; // skip $
+    i++;
     int len = 0;
     while (isdigit(s[i]))
-    {
-      len = len * 10 + (s[i] - '0');
-      i++;
-    }
-    i += 2; // skip \r\n
+      len = len * 10 + (s[i++] - '0');
+    i += 2;
     std::string curr = s.substr(i, len);
     tokens.push_back(curr);
-    i += len + 2; // skip data + \r\n
+    i += len + 2;
   }
+
   return tokens;
 }
